308ae3df50f12b7fec305b743b18b251d6d80840
Induce <aa5619721@gmail.com>
2024-02-20 22:33:10 +0300

    1 работа

--- Задание 2 семестр/1_Matrix.cpp
+++ Задание 2 семестр/1_Matrix.cpp
@@ -53,21 +53,61 @@ int Min_n(int a[][N]){
     return mn;
 }
 
+void Sedlo(int a[][N]){
+    for(int x = 0; x < N;x++){
+        int mx_m = 0;
+        int point_m = 0;
+        for(int i = 0; i < N;i++){
+            if(point_m < a[x][i]) {
+                mx_m = i;
+                point_m = a[x][i];
+            }
+        }
+        int mn_n = 0;
+        int point_n = INT_MAX;
+        for(int j = 0; j < N;j++){
+            if(point_n >= a[j][mx_m]){
+                point_n = a[j][mx_m];
+                mn_n = j;
+            }
+        }
 
+        if(a[x][mx_m] == a[mn_n][mx_m]){
+            std::cout << mn_n <<"," << mx_m << "\n";
+        }
+    }
+}
+
+void Sum_matrixs(int a[][N],int b[N]){
+    int sum[N] = {};
+
+    for(int i = 0; i < N;i++){
+        for(int j = 0; j < N;j++){
+            sum[i] +=  a[i][j] * b[j];
+        }
+    }
+    for(int i = 0;i < N;i++) std::cout << sum[i] << " ";
+}
 
 int main(){
-    int a[N][N] = {{7,6,5,4},
-                   {2,5,3,3},
-                   {9,8,7,2},
-                   {1,2,9,1}};
-    input(a);
-    output(a);
+    int a[N][N] = {{4,1,2,3},
+                   {7,1,5,1},
+                   {13,1,5,6},
+                   {12,1,5,7}
+                  };
+    int b[N] = {1,2,3,4};
+    //input(a);
+    //output(a);
 
-    std::cout << "\n" << "Trans" << "\n";
-    tranc(a);
-    output(a);
+    //std::cout << "\n" << "Trans" << "\n";
+    //tranc(a);
+    //output(a);
 
-    std::cout << "\n" << "MAX_M" << "\n" << Max_m(a);
-    std::cout << "\nMIN_N" << "\n" << Min_n(a);
+    //std::cout << "\n" << "MAX_M" << "\n" << Max_m(a);
+    //std::cout << "\nMIN_N" << "\n" << Min_n(a);
 
+    //std::cout << "\nSedlo_point" << "\n";
+    //Sedlo(a);
+
+    Sum_matrixs(a,b);
 }
